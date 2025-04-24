abeeac879a0e11d695bfffce0ad08fc2b917290b
HP <avinashdandime9890@gmail.com>
2025-04-24 22:30:34 +0530

    Food App First Commit

---
+++ app/src/main/java/com/example/foodapp/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.foodapp;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button login;
+    Button signup;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+     login = findViewById(R.id.btnLogin);
+     signup = findViewById(R.id.btnRegister);
+
+     login.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+             Toast.makeText(MainActivity.this, "Login has be clicked", Toast.LENGTH_SHORT).show();
+
+             Intent intent = new Intent(MainActivity.this,SignIn.class);
+            startActivity(intent);
+
+
+         }
+     });
+
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this,SingUp.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+
+
+    }
+}
